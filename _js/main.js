3dddf80a935ca63d163764bc1df6fdc33dec6b59
Lagna <antoine.heaj@gmail.com>
2015-07-23 21:13:17 +0200

    login page without php

---
+++ _js/main.js
@@ -0,0 +1,16 @@
+$(document).ready( function(){
+    
+    $(".cb-enable").click(function(){
+        var parent = $(this).parents('.switch');
+        $('.cb-disable',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', true);
+    });
+    $(".cb-disable").click(function(){
+        var parent = $(this).parents('.switch');
+        $('.cb-enable',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', false);
+    });
+     
+});
